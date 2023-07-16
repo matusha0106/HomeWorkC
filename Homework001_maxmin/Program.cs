c37dbd829d95536388fc4544f0b7cf79fe8dfe03
Anastasiya <podik0106@icloud.com>
2023-07-16 17:12:35 +0300

    Result min max

---
+++ Homework001_maxmin/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = -9;
+int b = -3;
+int max = a;
+int min = a;
+if (a > max) max = a;
+if (b > max) max = b;
+Console.Write ("max = ");
+Console.WriteLine (max);
+if (a < min) min = a;
+if (b < min) min = a;
+Console.Write ("min = ");
+Console.WriteLine (min);
