39208b8e680a762dbcd11cd6a5ed83079ae471d0
Anastasiya <podik0106@icloud.com>
2023-07-16 18:01:02 +0300

    Chetnoe , nechetmoe

---
+++ Homework003_Dev0/Program.cs
@@ -0,0 +1,27 @@
+﻿int a =4;
+int b = -3; 
+int c = 7;
+if ((a % 2) == 0)
+{
+        Console.WriteLine("Четное");
+}
+else
+{
+    Console.WriteLine("Не четное");
+}
+if ((b % 2) == 0)
+{
+    Console.WriteLine("Четное");
+}
+else
+{
+    Console.WriteLine("Не четное");
+}
+if ((c % 2) == 0)
+{
+    Console.WriteLine("Четное");
+}
+else
+{
+    Console.WriteLine("Не четное");
+}
